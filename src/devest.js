3983497861c9cbcf5050fe9a56a4b35ac3720a05
Vaeb <vaebmail@gmail.com>
2017-07-19 19:17:01 +0100

    Includes gamepass sales when calculating game estimates

--- src/devest.js
+++ src/devest.js
@@ -5,15 +5,15 @@ $(function() {
 
 	function formatNumber(number)
 	{
-	    number = number.toFixed(2) + '';
-	    x = number.split('.');
-	    x1 = x[0];
-	    x2 = x.length > 1 ? '.' + x[1] : '';
-	    var rgx = /(\d+)(\d{3})/;
-	    while (rgx.test(x1)) {
-	        x1 = x1.replace(rgx, '$1' + ',' + '$2');
-	    }
-	    return x1 + x2;
+		number = number.toFixed(2) + '';
+		var x = number.split('.');
+		var x1 = x[0];
+		var x2 = x.length > 1 ? '.' + x[1] : '';
+		var rgx = /(\d+)(\d{3})/;
+		while (rgx.test(x1)) {
+			x1 = x1.replace(rgx, '$1' + ',' + '$2');
+		}
+		return x1 + x2;
 	}
 
 	var $itemContainer = $("#item-container");
@@ -40,21 +40,53 @@ $(function() {
 		data: { assetId: assetID },
 		dataType: "json"
 	}).done(function(data) {
-		if (data.PriceInRobux == 0 || data.PriceInRobux == null) {
-			return;
+		var usdMade = 0;
+
+		if (data.PriceInRobux == null || data.PriceInRobux == 0) {
+			if (!isGame) return;
+		} else {
+			usdMade = taxRate * robuxToUsdRate * data.PriceInRobux * data.Sales;
 		}
 
-		var usdMade = taxRate * robuxToUsdRate * data.PriceInRobux * data.Sales;
-		var usdString = "$" + formatNumber(usdMade);
+		var gamePasses = isGame ? $('.gear-passes-asset') : [];
 
-		if (isGame) {
-			var $gameTitleContainer = $(".game-title-container");
-			$gameTitleContainer.append("<span class=text-label>Earnings: " + usdString + "</span>");
-		}
-		else {
-			var $priceContainerText = $(".price-container-text");
-			$priceContainerText.append("<div class='field-label text-label'>Earnings</div>");
-			$priceContainerText.append("<span>" + usdString + "</span>");
+		if (gamePasses.length > 0) {
+			var completed = 0;
+			var total = gamePasses.length;
+
+			for (var i = 0; i < gamePasses.length; i++) {
+				var passMatch = gamePasses[i].href.match(/library\/(\d+)/);
+				if (!passMatch) {
+					--total;
+					continue;
+				}
+
+				var passId = passMatch[1];
+
+				$.ajax({
+					url: 'https://api.roblox.com/Marketplace/ProductInfo',
+					data: { assetId: passId },
+					dataType: 'json',
+				}).done((data2) => {
+					if (data2.PriceInRobux != null && data2.PriceInRobux > 0) usdMade += taxRate * robuxToUsdRate * data2.PriceInRobux * data2.Sales;
+					if (++completed == total) {
+						var usdString = `$${formatNumber(usdMade)}`;
+						var $gameTitleContainer = $('.game-title-container');
+						$gameTitleContainer.append(`<span class=text-label>Earnings: ${usdString}</span>`);
+					}
+				});
+			}
+		} else {
+			var usdString = `$${formatNumber(usdMade)}`;
+
+			if (isGame) {
+				var $gameTitleContainer = $('.game-title-container');
+				$gameTitleContainer.append(`<span class=text-label>Earnings: ${usdString}</span>`);
+			} else {
+				var $priceContainerText = $('.price-container-text');
+				$priceContainerText.append("<div class='field-label text-label'>Earnings</div>");
+				$priceContainerText.append(`<span>${usdString}</span>`);
+			}
 		}
 	});
 });
